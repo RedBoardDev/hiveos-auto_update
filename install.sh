abcc3836e290a67b184809d572eeb97261beaf15
redboard <ott.thomas68@gmail.com>
2022-04-03 23:54:19 +0200

    add delay of 4h for execution

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ chmod 700 auto_update.sh
 mkdir log/
 
 echo "# Hiveos - Auto update" >> /hive/etc/crontab.root
-echo "00 */1 * * * /home/user/hiveos-auto_update/auto_update.sh >> /home/user/hiveos-auto_update/log/log.log" >> /hive/etc/crontab.root
+echo "00 */4 * * * /home/user/hiveos-auto_update/auto_update.sh >> /home/user/hiveos-auto_update/log/log.log" >> /hive/etc/crontab.root
